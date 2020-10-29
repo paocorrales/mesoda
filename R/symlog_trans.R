cdb392b4bf0f858a383e5a16c5cd64f0ace9d3e9
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2020-10-29 00:42:10 -0300

    Fix symlog_trans

--- R/symlog_trans.R
+++ R/symlog_trans.R
@@ -23,16 +23,16 @@ symlog_trans <- function(base = 10, thr = 1, scale = 1){
         sgn * unique(c(scales::pretty_breaks()(c(min(abs(x)), thr)),
                        scales::log_breaks(base)(c(max(abs(x)), thr))))
       else
-        sgn * log_breaks(base)(sgn * x)
+        sgn * scales::log_breaks(base)(sgn * x)
     } else {
       if(min(abs(x)) < thr)
-        unique(c(sgn * log_breaks()(c(max(abs(x)), thr)),
-                 pretty_breaks()(c(sgn * thr, x[which.min(abs(x))]))))
+        unique(c(sgn * scales::log_breaks()(c(max(abs(x)), thr)),
+                 scales::pretty_breaks()(c(sgn * thr, x[which.min(abs(x))]))))
       else
-        unique(c(-log_breaks(base)(c(thr, -x[1])),
-                 pretty_breaks()(c(-thr, thr)),
-                 log_breaks(base)(c(thr, x[2]))))
+        unique(c(-scales::log_breaks(base)(c(thr, -x[1])),
+                 scales::pretty_breaks()(c(-thr, thr)),
+                 scales::log_breaks(base)(c(thr, x[2]))))
     }
   }
-  trans_new(paste("symlog", thr, base, scale, sep = "-"), trans, inv, breaks)
+  scales::trans_new(paste("symlog", thr, base, scale, sep = "-"), trans, inv, breaks)
 }
