e451f630aa3772d9889dd9d7f119a95c53f2313f
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2021-03-09 19:09:41 -0300

    Saco Sao Borja por sondeo malo y lejos del resto

--- analysis/scripts/sondeos_est.R
+++ analysis/scripts/sondeos_est.R
@@ -39,6 +39,7 @@ for (m in seq_len(miembros)) {
     .[, iop := fcase(launch_time %between% c(ymd_hms("20181121150000"), ymd_hms("20181121210000")), "IOP07",
                      launch_time %between% c(ymd_hms("20181122140000"), ymd_hms("20181122200000")), "IOP08")] %>%
     .[variable %in% c("t", "td", "u", "v") & !str_detect(site, "/") & !is.na(fcst_value) & !is.na(iop)] %>%
+    .[site != "Sao Borja, Brazil"] %>%
     .[, lev := cut_round(alt, c(seq(0, 3000, 500), seq(4000, 21000, 1000)))]
 
 
