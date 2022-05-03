d863c561fe5c386ef574f47ee0cfb5b261cdcb49
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2022-05-02 21:44:34 -0300

    PM for dbz

--- R/probability_matching.R
+++ R/probability_matching.R
@@ -11,7 +11,7 @@
 #' @export
 
 
-calculate_probability_marching <- function(exp, run, ini_date, ciclos, acumulado = 1,
+calculate_probability_marching_pp <- function(exp, run, ini_date, ciclos, acumulado = 1,
                                            wrf_path = "/home/paola.corrales/datosmunin3/EXP/",
                                            out_path = "/home/paola.corrales/datosmunin3/EXP/derived_data/ppacum/") {
 
@@ -128,7 +128,146 @@ calculate_probability_marching <- function(exp, run, ini_date, ciclos, acumulado
   }
 }
 
+#' @rdname calculate_probability_marching_pp
+#'
+#'
+#' @export
+#'
+exp <- "E2"
+run <- "ana"
+dates <- lubridate::ymd_h("2018112210", "2018112213", "2018112216", "2018112219")
+calculate_probability_marching_dbz <- function(exp, run, dates,
+                                              wrf_path = "/home/paola.corrales/datosmunin3/EXP/",
+                                              out_path = "/home/paola.corrales/datosmunin3/EXP/derived_data/dbz/") {
 
+  library(reticulate)
+  use_condaenv("/home/jruiz/share/anaconda3/envs/wrfpy")
+  py_run_string("import os")
+  py_run_string("os.environ['PROJ_LIB'] = '/home/jruiz/share/anaconda3/share/proj'")
+  wrf <- import("wrf")
+  ncdf <- import("netCDF4")
+
+
+  # ini_date <- lubridate::ymd_hms(ini_date)
+
+  members <- 60
+  # dates <- seq.POSIXt(ini_date, by = "hour",
+  #                     length.out = ciclos)
+
+
+  if (run == "ana") {
+    pp_wrf_out <- purrr::map_dbl(dates, function(d) {
+
+      date <- d
+
+      print(d)
+
+      files_wrf <- list.files(path = paste0(wrf_path, exp, "/", toupper(run), "/", format(date, "%Y%m%d%H%M%S")),
+                              full.names = TRUE,
+                              recursive = TRUE,
+                              pattern = "analysis.mem*")
+
+      dbz_wrf <- purrr::map(files_wrf, function(f) {
+
+        metadatos <- unglue::unglue(basename(f), "analysis.mem0{mem}")
+
+        print(metadatos[[1]][["mem"]])
+
+        ncfile <-  ncdf$Dataset(f)
+        dbz_temp <- wrf$g_dbz$get_max_dbz(ncfile)$to_dataframe() %>%
+          setDT() %>%
+          .[, .(lon = XLONG, lat = XLAT, max_dbz)] %>%
+          .[, ":="(mem = metadatos[[1]][["mem"]])]
+
+        #
+        #
+        # metR::ReadNetCDF(f, vars = c(lon = "XLONG", lat = "XLAT")) %>%
+        #   .[, ":="(dbz = dbz_temp,
+        #            mem = metadatos[[1]][["mem"]])] %>%
+        #   .[, ":="(RAINNC = NULL,
+        #            RAINC = NULL,
+        #            RAINSH = NULL,
+        #            Time = NULL)]
+      }) %>%
+        data.table::rbindlist()
+
+
+      dbz_wrf_mean <- dbz_wrf[, .(ens_mean = mean(max_dbz)), by = .(lon, lat)]
+
+      dbz_wrf_mean[, rank := data.table::frank(-ens_mean, ties.method = "first")]
+
+      out <- dbz_wrf[, rank := data.table::frank(-max_dbz, ties.method = "first")] %>%
+        .[rank %in% metR::JumpBy(rank, by = members)] %>%
+        .[, .(rank, max_dbz)] %>%
+        .[, rank := data.table::frank(-max_dbz, ties.method = "first")] %>%
+        .[dbz_wrf_mean, on = "rank"] %>%
+        .[, ":="(exp = exp,
+                 date = date)] %>%
+        .[, c("x", "y") := wrf_project(lon, lat)]
+
+      saveRDS(out, paste0(out_path, exp, "_", run, "_", format(date, "%Y%m%d%H"), "_dbz_pmm.rds"))
+      d
+    })
+
+
+  } else if (run == "fcst") {
+
+    lead_time <- seq(acumulado, ciclos)
+
+    out <- purrr::map(lead_time, function(f) {
+
+      print(paste0(f, " of ", ciclos, " cycles"))
+
+      this_date <- ini_date + lubridate::hours(f)
+      lt <- c(f - acumulado, f)
+
+      pp_wrf <- map(lt, function(f){
+
+        file_wrf <- paste0(wrf_path, exp, "/", toupper(run), "/",
+                           format(ini_date, "%Y%m%d%H"), "/NPP/NPP_",
+                           format(ini_date, "%Y"), "-",
+                           format(ini_date, "%m"), "-",
+                           format(ini_date, "%d"), "_",
+                           format(ini_date, "%H"), "_FC",
+                           formatC(f, width = 2, flag = "0"),
+                           ".nc")
+
+
+
+        if (!file.exists(file_wrf)) {
+          return(NULL)
+        }
+
+        metR::ReadNetCDF(file_wrf, vars = c("PP", "XLAT", "XLONG")) %>%
+          .[, ":="(lon = XLONG, lat = XLAT)] %>%
+          .[, ":="(XLONG = NULL, XLAT = NULL)] %>%
+          .[, ":="(exp = exp,
+                   date = ini_date + lubridate::hours(f))] %>%
+          data.table::setnames("ens", "mem")
+      }) %>%
+        data.table::rbindlist() %>%
+        .[, .(pp_acum =  diff(PP), date = this_date), by = .(mem, exp, lon, lat)] %>%
+        .[, c("x", "y") := wrf_project(lon, lat)]
+      # %>%
+
+      pp_wrf_mean <- pp_wrf[, .(ens_mean = mean(pp_acum)), by = .(lon, lat, date)]
+      pp_wrf_mean[, rank := data.table::frank(-ens_mean, ties.method = "first")]
+
+      pp_wrf[, rank := data.table::frank(-pp_acum, ties.method = "first")] %>%
+        .[rank %in% metR::JumpBy(rank, by = members)] %>%
+        .[, .(rank, pp_acum)] %>%
+        .[, rank := data.table::frank(-pp_acum, ties.method = "first")] %>%
+        .[pp_wrf_mean, on = "rank"] %>%
+        .[, ":="(exp = exp,
+                 ini_date = ini_date)] %>%
+        .[, c("x", "y") := wrf_project(lon, lat)] %>%
+        setnames("pp_acum", "pp_pmm")
+
+    }) %>% data.table::rbindlist()
+
+    saveRDS(out, paste0(out_path, exp, "_", run, "_", format(ini_date, "%Y%m%d%H"), "_pp_pmm.rds"))
+  }
+}
 
 
 
--- temp.R
+++ temp.R
@@ -1,114 +1,88 @@
-library(tidyverse)
-library(lubridate)
-library(data.table)
-library(unglue)
+# Calcula y guarda acumulados del pronóstico para todo el periodo
+
 library(metR)
+library(tidyverse)
+library(data.table)
+library(lubridate)
+library(unglue)
 library(mesoda)
-library(patchwork)
-source(here::here("temp_R/postprocesamiento.R"))
-
-map_arg <- rnaturalearth::ne_states(country = c("argentina"),
-                                    returnclass = "sf")
-map_limitrofes <- rnaturalearth::ne_countries(country = c("Brazil", "Chile", "Uruguay", "Paraguay", "Bolivia"), returnclass = "sf")
-
-geom_mapa <- function(fill = NA) {
-  list(geom_sf(data = map_arg, fill = fill, color = "black", size = 0.1, inherit.aes = FALSE),
-       geom_sf(data = map_limitrofes, fill = fill, color = "black", size = 0.1, inherit.aes = FALSE),
-       coord_sf(ylim = c(-42, -19), xlim = c(-76, -51)),
-       scale_x_longitude(ticks = 5),
-       scale_y_latitude(ticks = 5))
-}
-
-wrf_path <- "/home/paola.corrales/datosmunin/EXP/"
-ini_date <- ymd_hms("20181120180000")
-ciclos <- 67
-
-dates <- seq(ini_date, by = "hour",
-             length.out = ciclos)
 
 
+imerg_path <- "/home/paola.corrales/mesoda/analysis/data/derived_data/"
+wrf_path <- "/home/paola.corrales/datosmunin3/EXP/"
+exp <- "E2"
+run <- "fcst"
 
-for (d in seq_along(dates)) {
+ini_date <- ymd_hms("20181122000000")
+ciclos <- 37
+acumulado <- 1
 
-  print(dates[d])
-
-
-  files_ana <- Sys.glob(paste0(wrf_path, "E[2,5,6,8]/ANA/", format(dates[d], "%Y%m%d%H%M%S"), "/analysis.ensmean"))
-
-  files_gue <- Sys.glob(paste0(wrf_path, "E[2,5,6,8]/GUESS/", format(dates[d], "%Y%m%d%H%M%S"), "/wrfarw.ensmean"))
-
-  ana <- purrr::map(files_ana, function(f) {
-
-    metadatos <- unglue(f, "/home/paola.corrales/datosmunin/EXP/{exp}/ANA/{fecha}/analysis.ensmean")
+dates <- seq.POSIXt(ini_date + hours(acumulado), by = "hour",
+                    length.out = ciclos - acumulado)
 
 
-    ReadNetCDF(f, vars = c(lon = "XLONG", lat = "XLAT", u = "P")) %>%
-      .[, c("u", "v") := uvmet(f)] %>%
-      .[, ":="(exp = metadatos[[1]][["exp"]],
-               date = metadatos[[1]][["fecha"]])] %>%
-      .[bottom_top == 7]
+q <- c(1, 5, 10, 25, 50)
+
+# Inicio
+
+pp_wrf <- purrr::map(dates, function(d) {
+
+  print(d)
+  date <- d
+
+  lead_time <- as.numeric(difftime(date, ini_date, units = "hours"))
+
+  files_wrf <-  purrr::map(c(acumulado, 0), function(l) {
+    paste0(wrf_path, exp, "/", toupper(run), "/",
+           format(ini_date, "%Y%m%d%H"), "/NPP/NPP_",
+           format(ini_date, "%Y"), "-",
+           format(ini_date, "%m"), "-",
+           format(ini_date, "%d"), "_",
+           format(ini_date, "%H"), "_FC",
+           formatC(lead_time - l, width = 2, flag = "0"),
+           ".nc")
+  }) %>% unlist()
+
+
+  pp_wrf <- purrr::map(files_wrf, function(f) {
+
+    if (!file.exists(f)) {
+      return(NULL)
+    }
+
+    ReadNetCDF(f, vars = c("PP", "XLAT", "XLONG")) %>%
+      .[, ":="(lon = XLONG, lat = XLAT)] %>%
+      .[, ":="(XLONG = NULL, XLAT = NULL)] %>%
+      .[, ":="(exp = exp,
+               date = date,
+               t = basename(f))]
+
   }) %>%
     rbindlist() %>%
+    .[, .(pp_acum = diff(PP)), by = .(lon, lat, date, exp, ens)] %>%
     .[, c("x", "y") := wrf_project(lon, lat)]
 
-  guess <- purrr::map(files_gue, function(f) {
 
-    metadatos <- unglue(f, "/home/paola.corrales/datosmunin/EXP/{exp}/GUESS/{fecha}/wrfarw.ensmean")
+  pp_wrf_mean <- pp_wrf[, .(pp_acum = mean(pp_acum)),
+                        by = .(lat, lon, x, y, exp)] %>%
+    .[, date := d]
 
-    ReadNetCDF(f, vars = c(lon = "XLONG", lat = "XLAT", u = "P")) %>%
-      .[, c("u", "v") := uvmet(f)] %>%
-      .[, ":="(exp = metadatos[[1]][["exp"]],
-               date = metadatos[[1]][["fecha"]])] %>%
-      .[bottom_top == 7]
+
+  pp_prop <- purrr::map(q, function(f){
+
+    pp_wrf %>%
+      .[, .(prop = mean(pp_acum > f)), by = .(lon, lat, x, y, exp)] %>%
+      .[, umbral := f] %>%
+      .[, date := d]
+
   }) %>%
-    rbindlist() %>%
-    .[, c("x", "y") := wrf_project(lon, lat)] %>%
-    setnames(c("u", "v"), c("u_guess", "v_guess"))
+    rbindlist()
 
-  ana %>%
-    # .[lat %between% c(-34.5, -28.5) & lon %between% c(-66.5, -62.5)] %>%
-    ggplot(aes(x, y)) +
-    geom_contour_fill(aes(z = v, fill = stat(level_d)),
-                      proj = norargentina_lambert,
-                      breaks = seq(-20, 20, 3)) +
-    geom_contour2(aes(z = v),
-                  proj = norargentina_lambert,
-                  breaks = seq(-20, 20, 3), color = "white", size = 0.1) +
-    scale_fill_divergent(super = ScaleDiscretised,
-                         guide = guide_colorsteps(barwidth = 0.5,
-                                                  barheight = 15)) +
-    geom_mapa() +
-    facet_wrap(~exp) +
-    labs(caption = paste0("ANA | ", dates[d]), fill = "v (s = 7)") +
-    theme_minimal(base_size = 10) +
-    theme(panel.ontop = TRUE,
-          panel.grid = element_line(linetype = 3, size = 0.2)) +
+  list(pp_wrf_mean = pp_wrf_mean, pp_prop = pp_prop)
 
-    guess %>%
-    ana[, on = .NATURAL] %>%
-    .[, diff := v - v_guess] %>%
-    # .[lat %between% c(-34.5, -28.5) & lon %between% c(-66.5, -62.5)] %>%
-    ggplot(aes(x, y)) +
-    geom_contour_fill(aes(z = diff, fill = stat(level_d)),
-                      proj = norargentina_lambert,
-                      breaks = seq(-7, 7, 1)) +
-    geom_contour2(aes(z = diff),
-                  color = "white", size = 0.1,
-                  proj = norargentina_lambert,
-                  breaks = seq(-7, 7, 1)) +
-    scale_fill_divergent(super = ScaleDiscretised,
-                         #                      # limits = c(1, 30),
-                         guide = guide_colorsteps(barwidth = 0.5,
-                                                  barheight = 15)) +
-    geom_mapa() +
-    labs(caption = paste0("ANA - GUESS | ", dates[d]), fill = "v (s = 7)") +
-    facet_wrap(~exp) +
-    theme_minimal(base_size = 10) +
-    theme(panel.ontop = TRUE,
-          panel.grid = element_line(linetype = 3, size = 0.2)) +
+}) %>%
+  reduce(function(x, y) list(pp_wrf_mean = rbind(x$pp_wrf_mean, y$pp_wrf_mean),
+                             pp_prop = rbind(x$pp_prop, y$pp_prop)))
 
-    plot_layout(ncol = 2, widths = c(1, 1))
-
-  ggsave(paste0("/home/paola.corrales/campos/Vs7_", format(dates[d], "%Y%m%d%H%M%S"), ".png"), height = 4, width = 8)
-
-}
+saveRDS(pp_wrf$pp_prop, paste0(exp, "_fcst_", format(ini_date, "%Y%m%d%H"), "_prop_acum_", acumulado, "h.rds"))
+saveRDS(pp_wrf$pp_wrf_mean, paste0(exp, "_fcst_", format(ini_date, "%Y%m%d%H"), "_mean_acum_", acumulado, "h.rds"))
